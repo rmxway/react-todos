08876eb0bcc647ea140fd75726800055c7f24c6e
e.dovgan <e.dovgan@askona.ru>
2020-11-25 18:08:46 +0300

    adaptive: fix

--- src/scripts/components/navbar/styledNavbar.js
+++ src/scripts/components/navbar/styledNavbar.js
@@ -1,11 +1,7 @@
 import styled from 'styled-components';
 import { breakpoints } from 'styles/sc/media';
 import { darken } from 'polished';
-<<<<<<< HEAD
-import { Container } from 'styles/sc/base';
-=======
 import { Container, MotionButton } from 'styles/sc/base';
->>>>>>> 6d1c2a99aecc7a2a06d8ab6a513df2ecff646d1d
 import { motion } from 'framer-motion';
 
 export const Nav = styled.nav`
@@ -13,38 +9,21 @@ export const Nav = styled.nav`
     top: 0;
     left: 0;
     right: 0;
-<<<<<<< HEAD
-    z-index: ${(props) => props.theme.z.menu};
-    background-color: ${(props) => props.theme.primary};
-    padding: 10px 0;
-=======
     min-height: 60px;
     z-index: ${(props) => props.theme.z.menu};
     background-color: ${(props) => props.theme.primary};
     padding: 10px 0;
     display: flex;
     align-items: center;
->>>>>>> 6d1c2a99aecc7a2a06d8ab6a513df2ecff646d1d
     box-shadow: 0 5px 30px #fff4;
     transition: ${(props) => props.theme.transitions.default};
 
     ${Container} {
-<<<<<<< HEAD
-        width: auto;
-=======
->>>>>>> 6d1c2a99aecc7a2a06d8ab6a513df2ecff646d1d
         display: flex;
         align-items: center;
         justify-content: flex-start;
     }
 
-<<<<<<< HEAD
-    img {
-        height: 35px;
-        max-height: 100%;
-        max-width: 100%;
-        margin-right: 20px;
-=======
     ${MotionButton} {
         ${breakpoints.lessThan('sm')`
             margin-right: auto;
@@ -60,7 +39,6 @@ export const Nav = styled.nav`
         ${breakpoints.lessThan('sm')`
             display: none;
         `}
->>>>>>> 6d1c2a99aecc7a2a06d8ab6a513df2ecff646d1d
     }
 
     .desktop-menu {
@@ -78,11 +56,7 @@ export const Nav = styled.nav`
             position: relative;
             display: block;
             margin: 0;
-<<<<<<< HEAD
-            margin: 0 15px;
-=======
             margin: 0 10px;
->>>>>>> 6d1c2a99aecc7a2a06d8ab6a513df2ecff646d1d
 
             a {
                 display: block;
@@ -137,11 +111,7 @@ export const MenuButton = styled(motion.button)`
         display: flex;        
         flex-direction: column;
         justify-content: space-between;
-<<<<<<< HEAD
-        margin-right: auto;
-=======
         margin-right: 15px;
->>>>>>> 6d1c2a99aecc7a2a06d8ab6a513df2ecff646d1d
     `}
 `;
 
@@ -151,13 +121,8 @@ export const MobileMenu = styled(motion.div)`
     background-color: ${({ theme }) => darken(0.05, theme.bg)};
     top: 45px;
     box-shadow: 0 10px 10px #0004;
-<<<<<<< HEAD
-    left: 10px;
-    right: 10px;
-=======
     left: 0;
     min-width: 300px;
->>>>>>> 6d1c2a99aecc7a2a06d8ab6a513df2ecff646d1d
     padding: 20px;
     z-index: 100;
 
@@ -176,12 +141,8 @@ export const MobileMenu = styled(motion.div)`
             color: ${({ theme }) => theme.textColor};
 
             &.active {
-<<<<<<< HEAD
-                border-bottom: 2px solid white;
-=======
                 font-weight: 900;
                 color: ${(props) => props.theme.primary};
->>>>>>> 6d1c2a99aecc7a2a06d8ab6a513df2ecff646d1d
             }
         }
     }
